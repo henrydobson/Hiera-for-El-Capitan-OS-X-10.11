009c359ec08d482256a08134d6c492915ef0462d
henrydobson <henrydobson@me.com>
2016-04-08 17:51:28 +0100

    :(

--- .githubRelease.sh
+++ .githubRelease.sh
@@ -4,18 +4,18 @@ export GITHUB_TOKEN=$GH_TOKEN
 VERSION=`cat Makefile | grep PACKAGE_VERSION | cut -d= -f2`
 RELEASE_TAG="v$VERSION"
 
-git tag $RELEASE_TAG && git push git@github.com:henrydobson/OSX_Hiera.git --tags
+git tag $RELEASE_TAG && git push $GH_REPO --tags
 
 /usr/local/gopath/bin/github-release release \
     --user henrydobson \
-    --repo OSX_Hiera \
+    --repo $GH_REPO \
     --tag $RELEASE_TAG \
     --name "$BUILDKITE_PROJECT_SLUG" \
     --description "$BUILDKITE_MESSAGE"
 
 /usr/local/gopath/bin/github-release upload \
     --user henrydobson \
-    --repo OSX_Hiera \
+    --repo $GH_REPO \
     --tag $RELEASE_TAG \
     --name "$BUILDKITE_PROJECT_SLUG" \
     --label "$BUILDKITE_MESSAGE" \
