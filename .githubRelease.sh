10187f08d541168896ddce4fa65e2e0604629a51
henrydobson <henrydobson@me.com>
2016-04-08 16:00:07 +0100

    Not as self contained as I thought. Need to git tag before release too.

--- .githubRelease.sh
+++ .githubRelease.sh
@@ -4,12 +4,14 @@ export GITHUB_TOKEN=$GH_TOKEN
 VERSION=`cat Makefile | grep PACKAGE_VERSION | cut -d= -f2`
 RELEASE_TAG="v$VERSION"
 
+git tag $RELEASE_TAG && git push --tags
+
 /usr/local/gopath/bin/github-release release \
     --user henrydobson \
     --repo OSX_Hiera \
     --tag $RELEASE_TAG \
     --name "$BUILDKITE_PROJECT_SLUG" \
-    --description "$BUILDKITE_MESSAGE" 
+    --description "$BUILDKITE_MESSAGE"
 
 /usr/local/gopath/bin/github-release upload \
     --user henrydobson \
