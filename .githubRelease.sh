e5bc72d464b7d1d812b4abce48b55a8be953d1e5
henrydobson <henrydobson@me.com>
2016-04-08 15:55:54 +0100

    Improper use of github-release resulted in fail. Adding release followed by upload.

--- .githubRelease.sh
+++ .githubRelease.sh
@@ -4,10 +4,17 @@ export GITHUB_TOKEN=$GH_TOKEN
 VERSION=`cat Makefile | grep PACKAGE_VERSION | cut -d= -f2`
 RELEASE_TAG="v$VERSION"
 
+/usr/local/gopath/bin/github-release release \
+    --user henrydobson \
+    --repo OSX_Hiera \
+    --tag $RELEASE_TAG \
+    --name "$BUILDKITE_PROJECT_SLUG" \
+    --description "$BUILDKITE_MESSAGE" 
+
 /usr/local/gopath/bin/github-release upload \
     --user henrydobson \
     --repo OSX_Hiera \
     --tag $RELEASE_TAG \
     --name "$BUILDKITE_PROJECT_SLUG" \
-    --description "$BUILDKITE_MESSAGE" \
+    --label "$BUILDKITE_MESSAGE" \
     --file $BUILDKITE_ARTIFACT_PATHS
