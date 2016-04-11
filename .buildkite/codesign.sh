968472a5599456d4b681ec0b3e1556e4503bd4a0
henrydobson <henrydobson@me.com>
2016-04-11 16:12:48 +0100

    making changes in /tmp. need sudo

--- .buildkite/codesign.sh
+++ .buildkite/codesign.sh
@@ -5,6 +5,6 @@ BUILDKITE_ARTIFACT_PATHS='/tmp/hiera.pkg'
 
 for f in $BUILDKITE_ARTIFACT_PATHS; do
   echo "Codesigning $f"
-  mv $f "$f-unsigned"
-  productsign --sign pebble.it "$f-unsigned" $f
+  sudo mv $f "$f-unsigned"
+  sudo productsign --sign pebble.it "$f-unsigned" $f
 done
--- .buildkite/seticon.sh
+++ .buildkite/seticon.sh
@@ -3,5 +3,5 @@ set -eo pipefail
 
 BUILDKITE_ARTIFACT_PATHS='/tmp/hiera.pkg'
 
-Rez -append icon.rsrc -o $BUILDKITE_ARTIFACT_PATHS
-SetFile -a C $BUILDKITE_ARTIFACT_PATHS
+sudo Rez -append icon.rsrc -o $BUILDKITE_ARTIFACT_PATHS
+sudo SetFile -a C $BUILDKITE_ARTIFACT_PATHS
