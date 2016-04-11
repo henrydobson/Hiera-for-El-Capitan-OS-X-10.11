a5dd0a2a0fe06b977926723a5a6b49bc1b396668
henrydobson <henrydobson@me.com>
2016-04-11 18:38:39 +0100

    Added the path to PATH but buildkite will need to restart to pick it up so I'm using the full path. It's better practice anyway.

--- .buildkite/automunkiimport.sh
+++ .buildkite/automunkiimport.sh
@@ -3,4 +3,4 @@ set -eo pipefail
 
 BUILDKITE_ARTIFACT_PATHS='/tmp/hiera.pkg'
 
-munkiimport --subdirectory apps/PuppetLabs/Hiera -c testing --verbose --nointeractive $BUILDKITE_ARTIFACT_PATHS
+/usr/local/munki/munkiimport --subdirectory apps/PuppetLabs/Hiera -c testing --verbose --nointeractive $BUILDKITE_ARTIFACT_PATHS
