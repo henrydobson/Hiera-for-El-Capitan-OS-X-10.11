1293f300d167c0498a3593caf35d6bb336e721ce
henrydobson <henrydobson@me.com>
2016-04-11 16:09:30 +0100

    Codesign: handle paths better

--- .buildkite/codesign.sh
+++ .buildkite/codesign.sh
@@ -5,6 +5,6 @@ BUILDKITE_ARTIFACT_PATHS='/tmp/hiera.pkg'
 
 for f in $BUILDKITE_ARTIFACT_PATHS; do
   echo "Codesigning $f"
-  mv $f "unsigned-$f"
-  productsign --sign pebble.it "unsigned-$f" $f
+  mv $f "$f-unsigned"
+  productsign --sign pebble.it "$f-unsigned" $f
 done
--- .buildkite/requires.sh
+++ .buildkite/requires.sh
@@ -2,4 +2,4 @@
 set -eo pipefail
 git clone git://github.com/henrydobson/luggage.git henrydobson/luggage
 cd henrydobson/luggage
-make bootstrap_files
+sudo make bootstrap_files
